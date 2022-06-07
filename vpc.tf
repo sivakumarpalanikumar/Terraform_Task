01dd91e3def0db38dfe0bf36c97445e9fdc9f7f5
Sivakumar P <72782266+sivakumarpalanikumar@users.noreply.github.com>
2022-06-07 17:55:57 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-/*provider "aws" {
+provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.region}"
@@ -8,7 +8,9 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc-cidr
   instance_tenancy     = "default"
   enable_dns_hostnames = true
+  
+    #create tags for cost evaluation purpose
   tags                 = { 
       Name = "terraform" 
       }
-}*/
+}
