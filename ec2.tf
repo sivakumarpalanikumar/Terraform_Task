753dbd18eb24aeb7ccfce6e2db2ccd7c43b31732
Sivakumar P <72782266+sivakumarpalanikumar@users.noreply.github.com>
2022-06-08 18:33:56 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  for_each = toset(["public", "private"])
+
+  name = "instance-${each.key}"
+
+  ami                    = ""
+  instance_type          = "t2.micro"
+  /*user_data =           << EOF
+		#! /bin/bash
+        sudo su
+        yum install update
+		yum install httpd
+	EOF*/
+  monitoring             = true
+  vpc_security_group_ids = [""]
+  subnet_id              = aws_subnet.publicsubnet.id
+  associate_public_ip_address = "true"
+
+  tags = {
+    name   = "terraform"
+    env = "prod"
+  }
+}
